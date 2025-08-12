6164b232499ac50844ae8f7a9953589ea2717ad0
omarfaruk-dev <omarfaruk.dev@gmail.com>
2025-08-12 16:53:28 +0600

    projects updated

--- src/components/Home/Projects.jsx
+++ src/components/Home/Projects.jsx
@@ -9,8 +9,8 @@ const Projects = () => {
     <section id="projects" className="py-8 md:py-12 lg:py-16">
       <div className="w-full md:w-1/2 mx-auto mb-16" data-aos="fade-up" data-aos-delay="200">
         <div className="divider before:bg-primary after:bg-primary text-primary text-3xl font-bold space-grotesk-font">
-        <h2>My Projects</h2>
-      </div>
+          <h2>My Projects</h2>
+        </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 my-10" data-aos="fade-up" data-aos-delay="300">
@@ -87,100 +87,111 @@ const Projects = () => {
       </div>
 
       {/* Modal */}
-{selectedProject && (
-  <dialog
-    id="project_modal"
-    className=" modal modal-open border border-primary/20 backdrop-blur-xs"
-    onClick={() => setSelectedProject(null)}
-  >
-    <div
-      className="modal-box max-w-3xl border border-primary/20"
-      onClick={(e) => e.stopPropagation()}
-    >
-             <div className="flex justify-between items-center mb-4">
-         <h3 className="font-bold text-2xl text-primary">
-           {selectedProject.title}
-         </h3>
-         <button
-           className="btn btn-circle btn-sm btn-outline btn-primary hover:bg-primary hover:text-base-100 hover:shadow-md hover:shadow-primary/20 transition-all"
-           onClick={() => setSelectedProject(null)}
-         >
-           <FaTimes />
-         </button>
-       </div>
+      {selectedProject && (
+        <dialog
+          id="project_modal"
+          className=" modal modal-open border border-primary/20 backdrop-blur-xs"
+          onClick={() => setSelectedProject(null)}
+        >
+          <div
+            className="modal-box max-w-3xl border border-primary/20"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="flex justify-between items-center mb-4">
+              <h3 className="font-bold text-2xl text-primary">
+                {selectedProject.title}
+              </h3>
+              <button
+                className="btn btn-circle btn-sm btn-outline btn-primary hover:bg-primary hover:text-base-100 hover:shadow-md hover:shadow-primary/20 transition-all"
+                onClick={() => setSelectedProject(null)}
+              >
+                <FaTimes />
+              </button>
+            </div>
 
-      {/* Thumbnail Image */}
-      <img
-        src={selectedProject.thumb_img}
-        alt={selectedProject.title}
-        className="md:w-full md:h-110 object-cover rounded-md mb-4 border border-base-300"
-      />
+            {/* Thumbnail Image */}
+            <img
+              src={selectedProject.thumb_img}
+              alt={selectedProject.title}
+              className="md:w-full md:h-110 object-cover rounded-md mb-4 border border-base-300"
+            />
 
-      {/* About */}
-      <p className="mb-4 text-sm text-secondary/60">{selectedProject.about}</p>
+            {/* About */}
+            <p className="mb-4 text-sm text-secondary/60">{selectedProject.about}</p>
 
-      {/* Description */}
-      <p className="mb-4 text-secondary/80">
-      <span className="font-semibold text-primary">Details:</span>{" "}
-        {selectedProject.description}</p>
+            {/* Description */}
+            <p className="mb-4 text-secondary/80">
+              <span className="font-semibold text-primary">Details:</span>{" "}
+              {selectedProject.description}</p>
 
-      {/* Challenges */}
-      <p className="mb-4 text-secondary/80">
-        <span className="font-semibold text-primary">Challenges:</span>{" "}
-        {selectedProject.challenges}
-      </p>
-      {/* Challenges overcome */}
-      <p className="mb-4 text-secondary/80">
-        <span className="font-semibold text-primary">Solutions Implemented:</span>{" "}
-        {selectedProject.overcome}
-      </p>
+            {/* Challenges */}
+            <p className="mb-4 text-secondary/80">
+              <span className="font-semibold text-primary">Challenges:</span>{" "}
+              {selectedProject.challenges}
+            </p>
+            {/* Challenges overcome */}
+            <p className="mb-4 text-secondary/80">
+              <span className="font-semibold text-primary">Solutions Implemented:</span>{" "}
+              {selectedProject.overcome}
+            </p>
+            {/* Potential Improvements and Future Plans */}
+            <p className="mb-4 text-secondary/80">
+              <span className="font-semibold text-primary">Potential Improvements and Future Plans:</span>{" "}
+              {selectedProject.PotentialImprovementsAndFuturePlans}
+            </p>
+            {/* Technologies */}
+            <div className="mt-4 flex flex-wrap gap-2 mb-4">
+            <span className="font-semibold text-primary">Technologies:</span>{" "}
+              {selectedProject.technology.map((tech, i) => (
+                <span
+                  key={i}
+                  className="badge badge-outline badge-primary text-xs font-medium px-3 py-1 border-primary/40 text-primary/80 hover:bg-primary/10 hover:border-primary/60 transition-all duration-200"
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
 
-      {/* Technologies */}
-      <div className="mb-4">
-        <span className="font-semibold">Technologies:</span>{" "}
-        {selectedProject.technology.join(", ")}
-      </div>
+            {/* Buttons */}
+            <div className="flex flex-wrap items-center justify-center gap-3 text-sm pt-5">
+              <a
+                href={selectedProject.live}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-sm btn-primary text-base-100 hover:shadow-md hover:shadow-primary/20 transition-all"
+              >
+                <FaGlobe className="mr-1" /> Live
+              </a>
+              <a
+                href={selectedProject.client}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-sm btn-outline btn-primary hover:shadow-md hover:text-base-100 hover:shadow-primary/20 transition-all"
+              >
+                <FaGithub /> Client
+              </a>
+              <a
+                href={selectedProject.server}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-sm btn-outline btn-primary hover:shadow-md hover:text-base-100 hover:shadow-primary/20 transition-all"
+              >
+                <FaGithub /> Server
+              </a>
+            </div>
 
-             {/* Buttons */}
-       <div className="flex flex-wrap items-center justify-center gap-3 text-sm mb-4">
-         <a
-           href={selectedProject.live}
-           target="_blank"
-           rel="noopener noreferrer"
-           className="btn btn-sm btn-primary text-base-100 hover:shadow-md hover:shadow-primary/20 transition-all"
-         >
-           <FaGlobe className="mr-1" /> Live
-         </a>
-         <a
-           href={selectedProject.client}
-           target="_blank"
-           rel="noopener noreferrer"
-           className="btn btn-sm btn-outline btn-primary hover:shadow-md hover:text-base-100 hover:shadow-primary/20 transition-all"
-         >
-           <FaGithub /> Client
-         </a>
-         <a
-           href={selectedProject.server}
-           target="_blank"
-           rel="noopener noreferrer"
-           className="btn btn-sm btn-outline btn-primary hover:shadow-md hover:text-base-100 hover:shadow-primary/20 transition-all"
-         >
-           <FaGithub /> Server
-         </a>
-       </div>
-
-       {/* Close Button */}
-       <div className="modal-action">
-         <button
-           className="btn btn-primary btn-outline hover:text-base-100 hover:shadow-md hover:shadow-primary/20 transition-all"
-           onClick={() => setSelectedProject(null)}
-         >
-           Close
-         </button>
-       </div>      
-    </div>
-  </dialog>
-)}
+            {/* Close Button */}
+            <div className="modal-action">
+              <button
+                className="btn btn-primary btn-outline hover:text-base-100 hover:shadow-md hover:shadow-primary/20 transition-all"
+                onClick={() => setSelectedProject(null)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </dialog>
+      )}
 
     </section>
   );
